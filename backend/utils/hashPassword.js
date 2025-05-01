5345d5e93c520f6f17724510f03a4e2c40a63d5b
saisaranyam <saisaranyamyla2005@gmail.com>
2025-05-01 12:54:50 +0530

    Create hashPassword.js

---
+++ backend/utils/hashPassword.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = async (plainPassword) => {
+  const salt = await bcrypt.genSalt(10);         
+  const hashed = await bcrypt.hash(plainPassword, salt); 
+  return hashed;
+};
+
+module.exports = hashPassword;
