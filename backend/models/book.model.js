5f87d14e5a60eb2aa6794581c3d20ac98da4a2bf
[Anjali Tomar] <[anjali.theaj3366@gmail.com]>
2025-05-09 01:39:35 +0530

    Create Book model

---
+++ backend/models/book.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    genre: {
+      type: [String],
+      required: true,
+    },
+    description: {
+      type: String,
+      maxlength: 1500,
+    },
+    image: {
+      type: String,
+      default:
+        "https://plus.unsplash.com/premium_photo-1677187301535-b46cec7b2cc8?q=80&w=923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    publishYear: {
+      type: Number,
+      min: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
